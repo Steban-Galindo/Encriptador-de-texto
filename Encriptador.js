65951147459725afe8080bddbc685b96fb3e1b84
Steban-Galindo <132316620+Steban-Galindo@users.noreply.github.com>
2023-05-20 16:52:30 -0500

    Agregando detalles

--- Encriptador.js
+++ Encriptador.js
@@ -1,9 +1,12 @@
 const botonEncriptar = document.getElementById("BotonEncriptar");
 const botonDesencriptar = document.getElementById("BotonDesencriptar");
+const botoncopiar = document.getElementById("btCopiar");
 const sectionWait = document.getElementById("wait");
 botonEncriptar.addEventListener("click", encriptar);
 botonDesencriptar.addEventListener("click", desencriptar);
+botoncopiar.addEventListener("click", copiar);
 function encriptar() {
+  sectionWait.style.display = "none";
   var texto = document.getElementById("TextIn").value;
   var texto_encriptado = "";
   for (var i = 0; i < texto.length; i++) {
@@ -23,5 +26,24 @@ function encriptar() {
     }
   }
   document.getElementById("resultado").textContent = texto_encriptado;
+}
+function desencriptar() {
   sectionWait.style.display = "none";
+  var texto = document.getElementById("TextIn").value;
+  var resultado = texto;
+  resultado = resultado.replace(/ai/gi, "a");
+  resultado = resultado.replace(/enter/gi, "e");
+  resultado = resultado.replace(/imes/gi, "i");
+  resultado = resultado.replace(/ober/gi, "o");
+  resultado = resultado.replace(/ufat/gi, "u");
+  document.getElementById("resultado").textContent = resultado;
+}
+function copiar() {
+  var aux = document.createElement("input");
+  aux.setAttribute("value", document.getElementById("resultado").innerHTML);
+  document.body.appendChild(aux);
+  aux.select();
+  document.execCommand("copy");
+  document.body.removeChild(aux);
+  alert("Texto copiado correctamente");
 }
